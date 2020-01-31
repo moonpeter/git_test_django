9053db55948ebd4921eb61c1a027d3ed7c18a64a
hbyyy <qjaduddl94@gmail.com>
2020-01-31 16:47:10 +0900

    feat: post.views.py 작업중 - template 가 필요합니다!

--- config/post/views.py
+++ config/post/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from post.models import Post
 
 
 def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
     pass
+
